60e38860aed9cd213adc1b0e7d9d0f46b67e4075
vuqu4ngminh <solution.vuqu4ngminh@gmail.com>
2024-01-26 00:01:29 +0700

    Deploy Project

---
+++ src/pages/client/Cart.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeFromCart, clearCart } from '../../redux/reducers/cart-reducer';
+import Table from 'react-bootstrap/Table';
+import { Link, useNavigate } from "react-router-dom"
+import NavScroll from '../../components/Navbar';
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FormatNumber } from '../../Utils';
+
+const Cart = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const [address, setAddress] = useState('')
+  const cartItems = useSelector((state) => state.shoppingCart.cartItems);
+
+  const calculateTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  };
+
+  const handleSubmitCart = async (e) => {
+    e.preventDefault()
+    const userId = localStorage.getItem('id')
+    if (userId) {
+      try {
+        if (address.trim().length < 10) {
+          toast.error('Hãy nhập địa chỉ hợp lệ')
+          return
+        }
+        await axios.post(process.env.REACT_APP_ORDER_API + `add`, {
+          cartItems: cartItems,
+          total: calculateTotal(),
+          userId: userId,
+          address: address
+        })
+        dispatch(clearCart())
+        toast.success('Đặt hàng thành công')
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      navigate('/login')
+    }
+  }
+  const addAdress = async () => {
+    const { data: res } = await axios.get(process.env.REACT_APP_USER_API + `${localStorage.getItem('id')}`)
+    setAddress(res.address)
+  }
+  return (
+    <>
+      <NavScroll />
+      <div className='cart'>
+        {cartItems.length === 0 ? (
+          <div className='noItem'>
+            <i class="bi bi-bag-x"></i>
+            <p>Không có sản phẩm nào trong giỏ hàng.</p>
+            <Link className='btn btn-outline-dark' to='/'>Tiếp tục mua sắm</Link>
+          </div>
+        ) : (
+          <form onSubmit={handleSubmitCart}>
+            <h1 className='mb-3'>Giỏ Hàng</h1>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Tên Sản Phẩm</th>
+                  <th>Giá</th>
+                  <th>Số Lượng</th>
+                  <th>Thành Tiền</th>
+                  <th style={{ display: 'flex', justifyContent: 'center' }}></th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.name}</td>
+                    <td>{FormatNumber(String(item.price))} đ</td>
+                    <td>{item.quantity}</td>
+                    <td>{FormatNumber(String(item.price * item.quantity))} đ</td>
+                    <td style={{ display: 'flex', justifyContent: 'center' }}>
+                      <div >
+                        <button className='btn btn-outline-danger' onClick={() => dispatch(removeFromCart(item.id))}>Xóa</button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan='3'></td>
+                  <td>Tổng:</td>
+                  <td>{FormatNumber(String(calculateTotal()))} đ</td>
+                </tr>
+              </tfoot>
+            </Table>
+            <div className='mt-3'>
+              {(localStorage.getItem('id')) ? (
+                <div class="input-group mb-5">
+                  <input type="text" class="form-control" placeholder="Chọn địa chỉ nhận hàng" aria-label="Recipient's username" aria-describedby="basic-addon2"
+                    onChange={(e) => setAddress(e.target.value)}
+                    value={address}
+                  />
+                  <div class="input-group-append">
+                    <button onClick={() => addAdress()} class="btn btn-outline-dark" type="button">Sử dụng địa chỉ mặc định</button>
+                  </div>
+                </div>
+              ) : null}
+            </div>
+            <button className="btn btn-dark w-100 py-2" type="submit">Xác nhận</button>
+          </form>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Cart;
---
+++ src/pages/client/Register.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css";
+import CryptoJS from 'crypto-js';
+import axios from 'axios';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import NavScroll from '../../components/Navbar';
+import { checkEmail, checkPhone, FormatString } from '../../Utils';
+
+const Register = () => {
+    const navigate = useNavigate()
+    const [message, setMessage] = useState('')
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setMessage('')
+        setName(FormatString(name))
+        setEmail(FormatString(email))
+        setPhone(FormatString(phone))
+        if (!name || !email || !phone || !password) {
+            setMessage("Không được để trống")
+            return
+        }
+        if (!checkEmail(email)) {
+            setMessage("Email không đúng định dạng")
+            return
+        }
+        const { data: user } = await axios.post(process.env.REACT_APP_USER_API + `/find/email`, {
+            email: email
+        })
+        if (user.length !== 0) {
+            setMessage('Email đã được đăng ký')
+        } else if (checkPhone(phone)) {
+            await axios.post(process.env.REACT_APP_USER_API + `/add`, {
+                name: name,
+                phone: phone,
+                email: email,
+                address: " ",
+                password: CryptoJS.MD5(password).toString(),
+                role: "user"
+            })
+            toast.success('Đăng ký thành công')
+            setTimeout(() => {
+                navigate('/login')
+            }, 1000);
+        } else {
+            setMessage('Số điện thoại không đúng định dạng')
+        }
+    }
+    return (
+        <>
+            <NavScroll />
+            <div className='register'>
+                <form className='form-signup w-100 m-auto' onSubmit={handleSubmit}>
+                    <h1 class="h3 mb-3 fw-normal">Đăng Ký</h1>
+                    <div className='text-danger'>{message}</div>
+                    <div class="form-floating">
+                        <input type="text" value={name} onChange={(e) => { setName(e.target.value) }} class="form-control" id="Name" required />
+                        <label for="Name">Họ Và Tên:</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="text" value={phone} onChange={(e) => { setPhone(e.target.value) }} class="form-control" id="Phone" required />
+                        <label for="Phone">Số Điện Thoại:</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="email" value={email} onChange={(e) => { setEmail(e.target.value) }} class="form-control" id="Email" required />
+                        <label for="Email">Email:</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} class="form-control" id="Password" required />
+                        <label for="Password">Mật Khẩu:</label>
+                    </div>
+                    <button class="btn btn-dark w-100 py-2" type="submit" >Đăng Ký</button>
+                    <span>Đã có tài khoản? <Link to="/login">Đăng nhập</Link>
+                    </span>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default Register
